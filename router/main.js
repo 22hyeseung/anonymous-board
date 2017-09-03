49d78b05cf9fa57bcdb1b2425c9cc9091d1ab718
huusz <irinso93@gmail.com>
2017-09-03 23:05:16 +0900

    data 스키마 추가

--- router/main.js
+++ router/main.js
@@ -12,6 +12,18 @@ module.exports = function(app, fs)
       res.end(data);
     });
   })
+    
+
+  app.get('/article/:key', function(req, res){
+    // fs.readFile(__dirname + '/../data/contents.json', 'utf-8', function(err, data){
+    //   // 개별 게시글 데이터를 JSON 형태로 파싱, key값으로 가져온다.
+    //   let contents = JSON.parse(data)[req.params.key];
+    //   // 게시글 내용만 보낸다.
+    //   return contents.content;
+    // });
+    res.send(contents[req.params.key]);
+    // res.render('read')
+  });
 
   // app.get('/article/:id', function(req, res{
     
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // express 로드
 const express = require('express');
 const app = express();
-
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const fs = require('fs');
@@ -14,11 +13,29 @@ const sever = app.listen(3030, () => {
   console.log("Express server has started on port 3030")
 });
 
+// json 데이터 파일 로드
+function readJsonFile(filepath, encoding) {
+
+  if (typeof (encoding) == 'undefined') {
+    encoding = 'utf8';
+  }
+  const file = fs.readFile(filepath, encoding);
+  return JSON.parse(file);
+}
+
+function getConfig(file) {
+
+  const filepath = __dirname + '/../data/' + file;
+  return readJsonFile(filepath);
+}
+
+let contents = getConfig('contents.json');
+
 // static 파일
 app.use(express.static('public'));
 
 // body parser
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 // session
 app.use(session({
   secret: '@#@#$huusz#@$#$',
