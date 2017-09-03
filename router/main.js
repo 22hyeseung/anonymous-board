c13d5fa5de11bd67665dfbc8f567d0fda1bc287e
huusz <irinso93@gmail.com>
2017-09-04 01:40:20 +0900

    개별 게시글 조회 페이지 (article/:key 페이지 성공!)

--- router/main.js
+++ router/main.js
@@ -1,54 +1,50 @@
+// json 데이터 파일 로드
+function readJsonFileSync(filepath, encoding) {
+  if (typeof (encoding) == 'undefined') {
+    encoding = 'utf8';
+  }
+  const file = fs.readFileSync(filepath, encoding);
+  if (file) {
+    return JSON.parse(file);
+  }
+}
+
+function getConfig(file) {
+  const filepath = __dirname + "/../data/" + file;
+  return readJsonFileSync(filepath);
+}
+
+let contents = getConfig("contents.json");
+// console.log(contents.si12ey["content"]);
+
 module.exports = function (app, fs) {
-  app.get('/', function (req, res) {
+  app.get('/index', function (req, res) {
     res.render('index')
   });
 
-  app.get('/article', function (req, res) {
-    res.render('read')
-    console.log('read screen is rendered.')
-    fs.readFile(__dirname + "/../data/" + "contents.json", 'utf-8', function (err, data) {
-      console.log(data);
-      res.end(data);
-    });
-  })
 
-  // json 데이터 파일 로드
-  function readJsonFileSync(filepath, encoding) {
-    if (typeof (encoding) == 'undefined') {
-      encoding = 'utf8';
-    }
-    const file = fs.readFileSync(filepath, encoding);
-    if (file) {
-      return JSON.parse(file);
-    }
+app.get('/article/:key', function (req, res) {
+  let key = req.params.key;
+  // console.log(contents.si12ey["id"]);
+  // console.log(key);
+  if (key) {
+    let content = contents[key];
+    res.render('read', { content });
+  } else {
+    res.status(404);
+    res.send('404 Not Found')
   }
+});
 
-  function getConfig(file) {
-    const filepath = __dirname + "/../data/" + file;
-    return readJsonFileSync(filepath);
-  }
 
-  let contents = getConfig("contents.json");
-  // console.log(contents.si12ey["content"]);
-  
-  app.get('/article/:key', function (req, res) {
-    res.send(contents[req.params.key]);
-    let content = contents.key["content"];
-    res.render('read');
-  });
+app.get('/post', function (req, res) {
+  res.render('write');
+  console.log('writing page is rendered.');
+})
 
-  // app.get('/article/:id', function(req, res{
-
-  // }));
-
-  app.get('/post', function (req, res) {
-    res.render('write')
-    console.log('write screen is rendered.')
-  })
-
-  app.get('/admin', function (req, res) {
-    res.render('admin')
-    console.log('admin screen is rendered.')
-  })
+app.get('/admin', function (req, res) {
+  res.render('admin');
+  console.log("admin's page is rendered.");
+})
 
 }
--- server.js
+++ server.js
@@ -27,3 +27,34 @@ app.use(session({
 }));
 
 const router = require('./router/main')(app, fs);
+
+// json 데이터 파일 로드
+function readJsonFileSync(filepath, encoding) {
+  if (typeof (encoding) == 'undefined') {
+    encoding = 'utf8';
+  }
+  const file = fs.readFileSync(filepath, encoding);
+  if (file) {
+    return JSON.parse(file);
+  }
+}
+
+function getConfig(file) {
+  const filepath = __dirname + "/data/" + file;
+  return readJsonFileSync(filepath);
+}
+
+let contents = getConfig("contents.json");
+// console.log(contents.si12ey["content"]);
+
+app.get('/article/:key', function (req, res) {
+  // res.send(contents[req.params.key]);
+  // console.log(typeof(contents.content["id"]));
+  // console.log(typeof(req.params.key));
+  if (req.params.key === contents.content["id"]) {
+    res.render('read', {contents});
+  } else {
+    res.status(404);
+    res.send('404 Not Found')
+  }
+});
